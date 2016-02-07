c900281e54f0275474338af4b4bb1b1180dadb31
BriungRi <brnli7@gmail.com>
2016-02-07 08:41:13 -0500

    Made UI adjustments, new app icon, and fixed editText em tags

--- app/src/main/java/com/brianvli/smelliandroid/GenerateActivity.java
+++ app/src/main/java/com/brianvli/smelliandroid/GenerateActivity.java
@@ -1,6 +1,9 @@
 package com.brianvli.smelliandroid;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.PaintDrawable;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -21,6 +24,7 @@ public class GenerateActivity extends AppCompatActivity implements View.OnClickL
     Button bGenerate;
     EditText etWord;
     TextView tvSentences;
+    private int intRot = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +34,7 @@ public class GenerateActivity extends AppCompatActivity implements View.OnClickL
         bGenerate = (Button) findViewById(R.id.bGenerate);
         etWord = (EditText) findViewById(R.id.etWord);
         tvSentences = (TextView) findViewById(R.id.tvSentences);
-
+        etWord.setHint("Enter any word ");
         bGenerate.setOnClickListener(this);
     }
 
@@ -43,12 +47,50 @@ public class GenerateActivity extends AppCompatActivity implements View.OnClickL
                     if (view != null) {
                         InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
                         imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+
                     }
+                    changeButtonColor();
                     tvSentences.setText(new PunTask().execute(etWord.getText().toString()).get());
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
                 break;
         }
+    }
+
+    private void changeButtonColor() {
+        String red = "00";
+        String green = "00";
+        String blue = "00";
+        switch(intRot % 7){
+            case 0:
+                red = "50";
+                break;
+            case 1:
+                green = "50";
+                break;
+            case 2:
+                blue = "50";
+                break;
+            case 3:
+                red = blue = "50";
+                break;
+            case 4:
+                bGenerate.setTextColor(Color.WHITE);
+                red = blue = "50";
+                break;
+            case 5:
+                green = blue = "50";
+                break;
+            case 6:
+                red = blue = green = "50";
+                break;
+        }
+        intRot++;
+        String strColor = "#" + red + green + blue;
+//        int initColor = ((ColorDrawable)bGenerate.getBackground()).getColor();
+//        String hexInitColor = Integer.toHexString(initColor);
+        Log.d("Button Color: ", "" + strColor);
+        bGenerate.setBackgroundColor(Color.parseColor(strColor));
     }
 }
--- app/src/main/java/com/brianvli/smelliandroid/PunTask.java
+++ app/src/main/java/com/brianvli/smelliandroid/PunTask.java
@@ -8,9 +8,6 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 
-/**
- * Created by brian on 2/5/16.
- */
 public class PunTask extends AsyncTask<String, Void, String> {
     @Override
     protected String doInBackground(String... params) {
@@ -20,7 +17,7 @@ public class PunTask extends AsyncTask<String, Void, String> {
             String url = "http://brianvli.com:5000/" + word;
             URLConnection uc = new URL(url).openConnection();
             BufferedReader br = new BufferedReader(new InputStreamReader(uc.getInputStream(), "UTF-8"));
-            String s = "";
+            String s;
             while ((s = br.readLine()) != null) {
                 output += s + "\n";
             }
@@ -29,12 +26,23 @@ public class PunTask extends AsyncTask<String, Void, String> {
             e.printStackTrace();
         }
         Log.d("Sentence", output);
-        return output;
+        return clean(output);
     }
 
     @Override
     protected void onPostExecute(String s) {
         super.onPostExecute(s);
 
+    }
+
+    private String clean(String s){
+        String[] elements = s.split("PUNNIFIED: ");
+        int beg = elements[0].indexOf("<em>");
+        int end = elements[0].indexOf("</em>");
+        String wordAt = elements[0].substring(beg + 4, end).toUpperCase();
+        elements[0] = elements[0].substring(0, beg) + wordAt + elements[0].substring(end + 5); //removes <em> tags and capitalizes word
+        String output = "";
+        output += elements[0] + "\n" + "PUNNIFIED: " + elements[1]; //adds space between sentences
+        return output;
     }
 }
